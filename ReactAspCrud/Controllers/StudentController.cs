14e956d3504253ee1133a73231d6a9710908900a
tsurubaso <tsurubaso@gmail.com>
2023-06-23 17:16:53 +0900

    Add project files.

---
+++ ReactAspCrud/Controllers/StudentController.cs
@@ -0,0 +1,94 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ReactAspCrud.Models;
+using VisioForge.GStreamer.API;
+
+namespace ReactAspCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentDbContext _studentDbContext;
+
+        public StudentController(StudentDbContext studentDbContext)
+        {
+
+
+            _studentDbContext = studentDbContext;
+        }
+
+        [HttpGet]
+        [Route("GetStudent")]
+        public async Task<IEnumerable<Student>> GetStudents()
+        {
+            return await _studentDbContext.Students.ToListAsync();
+        }
+
+        [HttpPost]
+        [Route("AddStudent")]
+        public async Task<Student> AddStudent(Student objStudent)
+
+        {
+            _studentDbContext.Students.Add(objStudent);
+            await _studentDbContext.SaveChangesAsync();
+            return objStudent;
+        }
+
+        [HttpPatch]
+        [Route("UpdateStudent/{id}")]
+        public async Task<Student> UpdateStudent(int id, Student objStudent)
+
+        {
+            var existingStudent = await _studentDbContext.Students.FindAsync(id);
+            if (existingStudent != null)
+            {
+                // Update specific properties of the existingStudent object
+                existingStudent.stname = objStudent.stname;
+                existingStudent.course = objStudent.course;
+
+                await _studentDbContext.SaveChangesAsync();
+                return existingStudent;
+            }
+            else
+            {
+                // Handle the case when the student with the given id is not found
+                // You can return an appropriate response or throw an exception
+                // for example: throw new ArgumentException("Student not found");
+                return null;
+            }
+        }
+
+
+        [HttpDelete]
+        [Route("DeleteStudent/{id}")]
+
+
+        public bool DeleteStudent(int id)
+
+        {
+            bool a = false;
+            var student =_studentDbContext.Students.Find(id);
+            if (student!= null) 
+            {
+                a = true;
+                _studentDbContext.Entry(student).State = EntityState.Deleted;
+                _studentDbContext.SaveChangesAsync();
+
+            }
+            else
+            {
+
+                a = false;
+            }
+           return a;
+        } 
+
+
+
+    }
+}
---
+++ ReactAspCrud/Models/StudentDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace ReactAspCrud.Models
+{
+    public class StudentDbContext : DbContext 
+    {
+       
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Data Source=theok\\SQLEXPRESS;Initial Catalog=StudentService2;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }                              //Data Source=theok\\SQLEXPRESS;Initial Catalog=StudentService2;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
